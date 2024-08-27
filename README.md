8daa21e38e89e1fc8da0ef12aa56be4a9339e7cf
Nelson Ameyo <156672683+nelson-pawait@users.noreply.github.com>
2024-08-27 13:48:01 +0300

    WIP - Adding basic routes - Adding Pydantic model - Updating README - Installing requests module

--- README.md
+++ README.md
@@ -11,3 +11,18 @@ To stop the Docker stack:
 ```
 docker compose down -v
 ```
+
+Sample Webhook Response
+
+```
+POST /test HTTP/1.1
+Host: inkomoko.requestcatcher.com
+Accept: */*
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+Content-Length: 1475
+Content-Type: application/json
+User-Agent: python-requests/2.28.1
+
+{"_id": 376087072, "formhub/uuid": "a7eb959ada4c485b8334ee761ab1e4a7", "starttime": "2024-08-27T11:06:06.920+03:00", "endtime": "2024-08-27T11:18:01.224+03:00", "cd_survey_date": "2024-08-27", "sec_a/unique_id": "SS01406240716104310", "sec_a/cd_biz_country_name": "Kenya", "sec_a/cd_biz_region_name": "Dadaab", "sec_b/bda_name": "Yunis Abdirahman", "sec_b/cd_cohort": "Cohort 2", "sec_b/cd_program": "Livelihood", "sec_c/cd_client_name": "ESPERANCE NYEYIMANA", "sec_c/cd_client_id_manifest": "106-00011657", "sec_c/cd_location": "Juba urban refugees", "sec_c/cd_clients_phone": "926985823", "sec_c/cd_phoneno_alt_number": "980326248", "sec_c/cd_clients_phone_smart_feature": "Smart phone Feature phone", "sec_c/cd_gender": "Female", "sec_c/cd_age": "28", "sec_c/cd_nationality": "Burundian", "sec_c/cd_strata": "Urban Based Refugee", "sec_c/cd_disability": "No", "sec_c/cd_education": "Finished high school/Graduate", "sec_c/cd_client_status": "New clients", "sec_c/cd_sole_income_earner": "Yes", "sec_c/cd_howrespble_pple": "4", "group_mx5fl16/cd_biz_status": "Idea stage", "__version__": "vBfco72yRxvHQun3cF8HPK", "meta/instanceID": "uuid:eb336e5a-d988-4933-ad07-08d64ffbb115", "_xform_id_string": "aW9w8jHjn4Cj8SSQ5VcojK", "_uuid": "eb336e5a-d988-4933-ad07-08d64ffbb115", "_attachments": [], "_status": "submitted_via_web", "_geolocation": [null, null], "_submission_time": "2024-08-27T08:18:02", "_tags": [], "_notes": [], "_validation_status": {}, "_submitted_by": null}
+```
--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from typing import Union
+from fastapi import FastAPI, Request
+import requests
+import json
 
-from fastapi import FastAPI
+from .models import DataItem
 
 app = FastAPI()
 
@@ -8,3 +11,74 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "Wor"}
+
+
+@app.post("/register_webhook")
+def register_url():
+    url = "http://dev.inkomoko.com:1055/register_webhook"
+    # payload = json.dumps({"url": "https://inkomoko.loca.lt/api"})
+    payload = json.dumps({"url": "https://inkomoko.requestcatcher.com/test"})
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url, headers=headers, data=payload)
+    return json.loads(response.text)
+
+
+@app.get("/sample_data")
+def sample_data():
+    url = "https://kf.kobotoolbox.org/api/v2/assets/aW9w8jHjn4Cj8SSQ5VcojK/data.json"
+    payload = ""
+    headers = {
+        "Authorization": "Token f24b97a52f76779e97b0c10f80406af5e9590eaf",
+        "Cookie": "django_language=en",
+    }
+    response = requests.get(url, headers=headers, data=payload)
+    return json.loads(response.text)
+
+
+@app.post("/process_webhook")
+async def process_webhook(request: Request):
+    payload = await request.body()
+    payload = format_data_keys(json.loads(payload))
+
+    # Convert payload to DataItem
+    try:
+        data_item = DataItem(**payload)
+    except Exception as e:
+        return {"error": str(e)}
+
+    return data_item
+
+
+def format_data_keys(payload: dict) -> dict:
+    """
+    Format the keys in the given data dictionary by replacing '/' with '_' and
+    removing any leading and ending '_' and '__.
+
+    Args:
+        payload (dict): The dictionary to be formatted
+
+    Returns:
+        dict: The formatted dictionary
+    """
+
+    data = {}
+    for key, value in payload.items():
+        # Replace the / with _ in the key
+        new_key: str = key.replace("/", "_")
+
+        # Remove preceding __
+        if new_key.startswith("__"):
+            new_key = key[2:]
+
+        # Remove preceding _
+        if new_key.startswith("_"):
+            new_key = key[1:]
+
+        # Fix Version field
+        if new_key == "version__":
+            new_key = "version"
+
+        # Assemble data with corrected key
+        data[new_key] = value
+
+    return data
