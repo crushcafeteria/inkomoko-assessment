7d229de78a5ef854028211ff45e3905b16d3fa40
Nelson Ameyo <156672683+nelson-pawait@users.noreply.github.com>
2024-08-29 01:45:48 +0300

    Adding Docker step to deployment

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -8,5 +8,7 @@
 @task('deploy')
     cd {{ $PROJECT_ROOT }}
     git pull origin {{ $BRANCH }}
+    docker compose down -v
+    docker compose up -d --build --force-recreate
     echo "MESSAGE => Deployment complete!"
 @endtask
