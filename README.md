9e93cc56089a493dbb097dbf2d4533d931c18287
Nelson Ameyo <156672683+nelson-pawait@users.noreply.github.com>
2024-08-30 15:42:41 +0300

    Updating README

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
+# Introduction
+
 - Demo Link: <http://172.104.118.166:8000/dataset>
 - Swagger API Documentation: <http://172.104.118.166:8000/docs>
 - Adminer Link: <http://172.104.118.166:9999>
 
-## Running the application
+#### Setup
 
-This application is built with FastAPI library and Python Docker container.
+This application is built with FastAPI library, Python Docker container, Envoy TaskRunner and PyTest.
 
-#### Running the Docker stack
+#### Running the application
 
 To run the development server use the command below:
 
@@ -16,23 +18,68 @@ docker compose up -d --force-recreate --build
 
 You can inspect Python packages installed in the container by opening `requirements.txt`.
 
-#### Stopping the Docker stack
+#### Stopping the application
 
-To stop the Docker stack:
+To stop the application, take down the Docker stack:
 
 ```
 docker compose down -v
 ```
 
-You can inspect the Docker container laters by opening `Dockerfile`
+You can inspect the Docker container layers by opening `Dockerfile`
 
-## Previewing the data
+## Using the application
 
-You can access get all the items in the database in JSON format at this link:
+#### Accessing container environment
 
-- **Dataset Viewer** - <http://172.104.118.166:8000/dataset>
+This application can be accessed primarily from its REST API.
 
-## SQL Schema
+If you want to access the container logs, please use the command below:
+
+```
+docker compose logs web
+```
+
+If you want to access the container bash terminal, please use the command below:
+
+```
+docker compose exec web bash
+```
+
+#### How to register the webhook
+
+#### How to manually extract data from Kobo
+
+Toe manually extract data from the API:
+
+1. Access Swagger UI here: <http://172.104.118.166:8000/docs>
+2. Click on the `/extract_records` section to reveal.
+3. Next, click on `Try it out` then `Execute`
+4. You should see a positive response in a few moments
+5. Because the app processes data in the background, you should wait a few moments before checking for results.
+
+#### Previewing database contents
+
+To view your database contents, you have 3 options:
+
+1. Open Adminer UI here: <http://172.104.118.166:9999>
+2. Visit the `/dataset` URL here: <http://172.104.118.166:8000/dataset>
+3. Access `/dataset` route in the Swagger UI
+
+#### Receiving data in realtime with webhooks
+
+To receive data in realtime, you must register a webhook with the data source. To register a webhook URL:
+
+1. Access Swagger UI here: <http://172.104.118.166:8000/docs>
+2. Click on the `/register_webhook` section to reveal
+3. Enter your webhook URL and submit
+4. You should see a positive response in a few moments
+
+Now every time the form is filled, your registered webhook URL will receive a copy of the data.
+
+You can check your app received the data by previewing the database contents using the methods described above.
+
+## SQL Schema File
 
 A sample `schema.sql` fil has been attached in the project root. To import this file, you can access Adminer using the details below and use its Import feature:
 
@@ -41,12 +88,12 @@ A sample `schema.sql` fil has been attached in the project root. To import this 
 - Password: `root`
 - Database Name: `inkomoko`
 
-## Tunneling via `localtunnel`
+## Running Tests
 
-In order to receive requests on `localhost`, you need to run `localtunnel` to allow your local app accessible online with the following command.
+To run unit tests, use the following command:
 
 ```
-lt --port 8000 -s inkomoko
+pytest
 ```
 
-Please adjust the subdomain correctly and make sure to register it to receive webhook data.
+To inspect the unit tests, see the `test_main.py` file.
--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- Adminer 4.8.1 MySQL 11.5.2-MariaDB-ubu2404 dump
+-- Adminer 4.8.1 MySQL 11.4.2-MariaDB-ubu2404 dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -15,28 +15,8 @@ CREATE TABLE `dataset` (
   `starttime` datetime DEFAULT NULL,
   `endtime` datetime DEFAULT NULL,
   `cd_survey_date` varchar(254) DEFAULT NULL,
-  `sec_a_unique_id` varchar(254) DEFAULT NULL,
-  `sec_a_cd_biz_country_name` varchar(254) DEFAULT NULL,
-  `sec_a_cd_biz_region_name` varchar(254) DEFAULT NULL,
-  `sec_b_bda_name` varchar(254) DEFAULT NULL,
-  `sec_b_cd_cohort` varchar(254) DEFAULT NULL,
-  `sec_b_cd_program` varchar(254) DEFAULT NULL,
-  `sec_c_cd_client_name` varchar(254) DEFAULT NULL,
-  `sec_c_cd_client_id_manifest` varchar(254) DEFAULT NULL,
-  `sec_c_cd_location` varchar(254) DEFAULT NULL,
-  `sec_c_cd_clients_phone` varchar(254) DEFAULT NULL,
-  `sec_c_cd_phoneno_alt_number` varchar(254) DEFAULT NULL,
-  `sec_c_cd_clients_phone_smart_feature` varchar(254) DEFAULT NULL,
-  `sec_c_cd_gender` varchar(254) DEFAULT NULL,
-  `sec_c_cd_age` int(11) DEFAULT NULL,
-  `sec_c_cd_nationality` varchar(254) DEFAULT NULL,
-  `sec_c_cd_strata` varchar(254) DEFAULT NULL,
-  `sec_c_cd_disability` varchar(254) DEFAULT NULL,
-  `sec_c_cd_education` varchar(254) DEFAULT NULL,
-  `sec_c_cd_client_status` varchar(254) DEFAULT NULL,
-  `sec_c_cd_sole_income_earner` varchar(254) DEFAULT NULL,
-  `sec_c_cd_howrespble_pple` int(11) DEFAULT NULL,
-  `group_mx5fl16_cd_biz_status` varchar(254) DEFAULT NULL,
+  `cd_biz_status` varchar(254) DEFAULT NULL,
+  `bd_biz_operating` varchar(254) DEFAULT NULL,
   `version` varchar(254) DEFAULT NULL,
   `meta_instanceID` varchar(254) DEFAULT NULL,
   `xform_id_string` varchar(254) DEFAULT NULL,
@@ -53,11 +33,59 @@ CREATE TABLE `dataset` (
   KEY `ix_dataset_id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-INSERT INTO `dataset` (`id`, `external_id`, `formhub_uuid`, `starttime`, `endtime`, `cd_survey_date`, `sec_a_unique_id`, `sec_a_cd_biz_country_name`, `sec_a_cd_biz_region_name`, `sec_b_bda_name`, `sec_b_cd_cohort`, `sec_b_cd_program`, `sec_c_cd_client_name`, `sec_c_cd_client_id_manifest`, `sec_c_cd_location`, `sec_c_cd_clients_phone`, `sec_c_cd_phoneno_alt_number`, `sec_c_cd_clients_phone_smart_feature`, `sec_c_cd_gender`, `sec_c_cd_age`, `sec_c_cd_nationality`, `sec_c_cd_strata`, `sec_c_cd_disability`, `sec_c_cd_education`, `sec_c_cd_client_status`, `sec_c_cd_sole_income_earner`, `sec_c_cd_howrespble_pple`, `group_mx5fl16_cd_biz_status`, `version`, `meta_instanceID`, `xform_id_string`, `uuid`, `attachments`, `status`, `geolocation`, `submission_time`, `tags`, `notes`, `validation_status`, `submitted_by`) VALUES
-(5,	376888360,	'a7eb959ada4c485b8334ee761ab1e4a7',	'2024-08-29 02:06:06',	'2024-08-29 02:08:29',	'2024-08-29',	'SS01406240716145458',	'Kenya',	'Dadaab',	'Yunis Abdirahman',	'Cohort 2',	'Livelihood',	'NETSANET ADENE GEBREHIWET',	'106-00010320',	'Juba urban refugees',	'925956160',	NULL,	'Smart phone',	'Female',	32,	'Ethiopian',	'Urban Based Refugee',	'No',	'Attended high school',	'New clients',	'Yes',	3,	'Idea stage',	'vBfco72yRxvHQun3cF8HPK',	'uuid:e7b88b99-c2cb-4a08-bac8-681c54686be8',	'aW9w8jHjn4Cj8SSQ5VcojK',	'e7b88b99-c2cb-4a08-bac8-681c54686be8',	'[]',	'submitted_via_web',	'\"[null, null]\"',	'2024-08-28 23:08:32',	'[]',	'[]',	'\"{}\"',	NULL),
-(6,	376888732,	'a7eb959ada4c485b8334ee761ab1e4a7',	'2024-08-29 02:08:29',	'2024-08-29 02:10:46',	'2024-08-29',	'SS01406240716100553',	'Ethiopia',	'Addis Ababa',	'Estalu Baye',	'Cohort 2',	'Livelihood',	'NDIKURIYO EGIDE',	'106-00002884',	'Juba urban refugees',	'921814730',	'927360888',	'Smart phone Feature phone',	'Male',	33,	'Burundian',	'Urban Based Refugee',	'No',	'Attended high school',	'New clients',	'Yes',	5,	'Idea stage',	'vBfco72yRxvHQun3cF8HPK',	'uuid:d512b0d5-b1af-43ac-b3cf-00618215c51f',	'aW9w8jHjn4Cj8SSQ5VcojK',	'd512b0d5-b1af-43ac-b3cf-00618215c51f',	'[]',	'submitted_via_web',	'\"[null, null]\"',	'2024-08-28 23:10:50',	'[]',	'[]',	'\"{}\"',	NULL),
-(7,	376889084,	'a7eb959ada4c485b8334ee761ab1e4a7',	'2024-08-29 02:10:46',	'2024-08-29 02:14:03',	'2024-08-31',	'SS01406240716100553',	'Ethiopia',	'Addis Ababa',	'Ahmed Mohammed',	'Cohort 2',	'Livelihood',	'NDIKURIYO EGIDE',	'106-00002884',	'Juba urban refugees',	'921814730',	'927360888',	'Smart phone Feature phone',	'Male',	33,	'Burundian',	'Urban Based Refugee',	'No',	'Attended high school',	'New clients',	'Yes',	5,	'Idea stage',	'vBfco72yRxvHQun3cF8HPK',	'uuid:049d4ec5-23fc-41cd-8415-6d5d7be20bcc',	'aW9w8jHjn4Cj8SSQ5VcojK',	'049d4ec5-23fc-41cd-8415-6d5d7be20bcc',	'[]',	'submitted_via_web',	'\"[null, null]\"',	'2024-08-28 23:14:05',	'[]',	'[]',	'\"{}\"',	NULL),
-(8,	376889725,	'a7eb959ada4c485b8334ee761ab1e4a7',	'2024-08-29 02:14:03',	'2024-08-29 02:19:50',	'2024-08-29',	'SS01406240717130102',	'Ethiopia',	'Addis Ababa',	'Ahmed Mohammed',	'Cohort 2',	'Livelihood',	'SUWAD OMAR KUKU',	'243-00002864',	'Juba urban refugees',	'915137484',	NULL,	'Feature phone',	'Female',	51,	'Sudanese',	'Urban Based Refugee',	'No',	'Attended primary school',	'New clients',	'Yes',	8,	'Idea stage',	'vBfco72yRxvHQun3cF8HPK',	'uuid:f34a0c18-bb8a-44be-940d-da19266614da',	'aW9w8jHjn4Cj8SSQ5VcojK',	'f34a0c18-bb8a-44be-940d-da19266614da',	'[]',	'submitted_via_web',	'\"[null, null]\"',	'2024-08-28 23:19:52',	'[]',	'[]',	'\"{}\"',	NULL),
-(9,	376889869,	'a7eb959ada4c485b8334ee761ab1e4a7',	'2024-08-29 02:19:50',	'2024-08-29 02:20:57',	'2024-08-30',	'SS01406240717130102',	'South Sudan',	'Juba',	'Atong Grace Akuot',	'Cohort 2',	'Livelihood',	'SUWAD OMAR KUKU',	'243-00002864',	'Juba urban refugees',	'915137484',	NULL,	'Feature phone',	'Female',	51,	'Sudanese',	'Urban Based Refugee',	'No',	'Attended primary school',	'New clients',	'Yes',	8,	'Idea stage',	'vBfco72yRxvHQun3cF8HPK',	'uuid:03386603-52c4-4e79-9264-ea96b78eee3a',	'aW9w8jHjn4Cj8SSQ5VcojK',	'03386603-52c4-4e79-9264-ea96b78eee3a',	'[]',	'submitted_via_web',	'\"[null, null]\"',	'2024-08-28 23:20:59',	'[]',	'[]',	'\"{}\"',	NULL);
 
--- 2024-08-28 23:23:42
+DROP TABLE IF EXISTS `section_a`;
+CREATE TABLE `section_a` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sec_a_unique_id` varchar(254) DEFAULT NULL,
+  `sec_a_cd_biz_country_name` varchar(254) DEFAULT NULL,
+  `sec_a_cd_biz_region_name` varchar(254) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`),
+  KEY `ix_section_a_id` (`id`),
+  CONSTRAINT `section_a_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `dataset` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+
+DROP TABLE IF EXISTS `section_b`;
+CREATE TABLE `section_b` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sec_b_bda_name` varchar(254) DEFAULT NULL,
+  `sec_b_cd_cohort` varchar(254) DEFAULT NULL,
+  `sec_b_cd_program` varchar(254) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`),
+  KEY `ix_section_b_id` (`id`),
+  CONSTRAINT `section_b_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `dataset` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+
+DROP TABLE IF EXISTS `section_c`;
+CREATE TABLE `section_c` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sec_c_cd_client_name` varchar(254) DEFAULT NULL,
+  `sec_c_cd_client_id_manifest` varchar(254) DEFAULT NULL,
+  `sec_c_cd_location` varchar(254) DEFAULT NULL,
+  `sec_c_cd_clients_phone` varchar(254) DEFAULT NULL,
+  `sec_c_cd_phoneno_alt_number` varchar(254) DEFAULT NULL,
+  `sec_c_cd_clients_phone_smart_feature` varchar(254) DEFAULT NULL,
+  `sec_c_cd_gender` varchar(254) DEFAULT NULL,
+  `sec_c_cd_age` int(11) DEFAULT NULL,
+  `sec_c_cd_nationality` varchar(254) DEFAULT NULL,
+  `sec_c_cd_strata` varchar(254) DEFAULT NULL,
+  `sec_c_cd_disability` varchar(254) DEFAULT NULL,
+  `sec_c_cd_education` varchar(254) DEFAULT NULL,
+  `sec_c_cd_client_status` varchar(254) DEFAULT NULL,
+  `sec_c_cd_sole_income_earner` varchar(254) DEFAULT NULL,
+  `sec_c_cd_howrespble_pple` int(11) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`),
+  KEY `ix_section_c_id` (`id`),
+  CONSTRAINT `section_c_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `dataset` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+
+-- 2024-08-30 12:42:09
